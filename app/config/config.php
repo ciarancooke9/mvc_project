5984c87170ddc53e5731b017b9abe0613faa1ead
ciarancooke9 <63401191+ciarancooke9@users.noreply.github.com>
2022-04-29 17:18:54 +0100

    Bare Framework, ready for development into share posts application

--- app/config/config.php
+++ app/config/config.php
@@ -2,9 +2,9 @@
 
 //DB params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '123456');
-define('DB_NAME', 'mvc');
+define('DB_USER', '_YOUR_USER_');
+define('DB_PASS', '_YOUR_PASS_');
+define('DB_NAME', '_YOUR_DBNAME');
 
 
 
@@ -12,7 +12,7 @@ define('DB_NAME', 'mvc');
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL Root
-define('URLROOT', 'http://localhost/mvc_project/mvc_project');
+define('URLROOT', '_YOUR_URL_');
 
 //Site Name
-define('SITENAME', 'mvc_project');
+define('SITENAME', '_YOUR_SITE_NAME');
