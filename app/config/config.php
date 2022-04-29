5e3621fb24497227a3180a338f246ffef1db3ce6
ciarancooke9 <63401191+ciarancooke9@users.noreply.github.com>
2022-04-29 17:12:11 +0100

    section 4 complete

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,13 @@
 <?php
+
+//DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'mvc');
+
+
+
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
