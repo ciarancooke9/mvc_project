a91c0a8a5ef471e8417381b446626a0b0c2583b2
ciarancooke9 <63401191+ciarancooke9@users.noreply.github.com>
2022-04-29 14:55:39 +0100

    htaccess moved

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/mvc_project/mvc_project');
+
+//Site Name
+define('SITENAME', 'mvc_project');
