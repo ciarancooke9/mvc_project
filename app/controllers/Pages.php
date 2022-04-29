fa7c7d41ee421bcc6edb955ffff9ccc64c1fa8be
ciarancooke9 <63401191+ciarancooke9@users.noreply.github.com>
2022-04-29 13:54:25 +0100

    Section 3: Core Framework complete

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,6 +2,12 @@
 
 class Pages {
     public function __construct(){
-        echo 'Pages loaded';
+    }
+
+    public  function  index(){
+    }
+
+    public  function  about($id){
+        echo $id;
     }
 }
